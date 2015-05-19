22acc8fb6bb722f17573bda65ac072434eb8b5ff
Stefan Schinkel <mail@dreeg.org>
2015-05-19 21:55:04 +0200

    meshgrid implementation

--- ms.js
+++ ms.js
@@ -1,3 +1,40 @@
+function repmat(x, n) {
+  // repeat a value n times as an array
+  var arr = [];
+  var i;
+  for (i = 0; i < n; i++) { arr = arr.concat(x); }
+  return arr;
+}
+function range(n) {
+  // primitive range() function
+  var arr = [];
+  var i;
+  for (i = 0; i < n; i++) { arr[i] = i + 1; }
+  return arr;
+}
+function meshgrid(N) {
+  /* @function
+   *
+   * @description
+   * generate a rectangular 1:N grid in 2-D
+   *
+   * @param {Number} size of grid
+   *
+   * @return {Object} with x and y grid parts
+  */
+  var i;
+  var xGrid = [];
+  var yGrid = [];
+
+  for (i = 0; i < N; i++) {
+    xGrid[i] = range(N);
+    yGrid[i] = repmat(i + 1, N);
+  }
+  return {
+    x: xGrid,
+    y: yGrid
+  };
+}
 function renderBoard() {
   var N = 8; // no of scares
   var L = 32; // length of side
@@ -25,3 +62,5 @@ function renderBoard() {
 
   }
 }
+
+module.exports.meshgrid = meshgrid;
